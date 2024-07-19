4e7a0443aec7eca803d43d4616601697ba363949
Black  Alpha <148836568+CharukaMahesh@users.noreply.github.com>
2024-07-19 08:12:37 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,5 +1,5 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
+const GITHUB_AUTH_TOKEN = "https://github.com/CharukaMahesh/Suhail-Md/tree/main"
+const BOT_NUMBER = "94784745155"
 
 
 module.exports = {
